0c37dba48a54dc442682e5a7a231366cdc42b49c
gbl487 <gbl487@inlumine.ual.es>
2024-01-31 18:18:09 +0100

    Creando el formulario de registro e inicio de sesion

--- src/Controllers/context/registerContext.js
+++ src/Controllers/context/registerContext.js
@@ -1,8 +1,8 @@
 import { atom } from 'nanostores'
 
-export const register = atom(false)
+export const registerState = atom(false)
 
 export const setRegister = (e) => {
   e.preventDefault()
-  register.set(!register.value)
+  registerState.set(!registerState.value)
 }
--- src/Views/components/RegisterForm.jsx
+++ src/Views/components/RegisterForm.jsx
@@ -2,21 +2,34 @@ import { InputMask } from 'primereact/inputmask'
 import { checkDNI_NIE } from 'src/Controllers/utils/checkDNI-NIE'
 import { useState } from 'react'
 import { useStore } from '@nanostores/react'
-import { register, setRegister } from 'src/Controllers/context/registerContext'
+import {
+  registerState,
+  setRegister,
+} from 'src/Controllers/context/registerContext'
 // import firebaseApp from 'src/Model/Firebase'
-// import { getAuth, signInWithPhoneNumber,  } from 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/auth'
 // import { getFirestore, doc, collection, setDoc } from 'firebase/firestore'
-// const auth = getAuth(firebaseApp)
+import { useForm } from 'react-hook-form'
 
 export default function RegisterForm() {
-  const $register = useStore(register)
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    formState: { errors },
+  } = useForm()
+  const $registerState = useStore(registerState)
   // const firestore = getFirestore(firebaseApp)
+  // const auth = firebaseApp.getAuth()
 
   const [dni_nie, setDni_nie] = useState('')
   const [phone, setPhone] = useState('')
   const [errorDNI, setErrorDNI] = useState(null)
   const handleDNI_NIE = (e) => {
     const value = e.target.value
+    if (value.length > 9) return
+    setDni_nie(value)
     const primerCaracter = value.charAt(0)
     if (
       primerCaracter !== '' &&
@@ -28,10 +41,8 @@ export default function RegisterForm() {
       setErrorDNI('El primer caracter debe ser X, Y, Z, o un número')
     } else {
       setErrorDNI(null)
-      setDni_nie(value)
       if (value.charAt(8) !== '_' && value.charAt(8) !== '') {
         const { valid } = checkDNI_NIE({ value })
-        console.log(valid)
         if (valid !== true) {
           setErrorDNI('El DNI/NIE introducido no es válido')
         } else {
@@ -41,62 +52,73 @@ export default function RegisterForm() {
     }
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+  const onSubmit = (data) => console.log(data)
   return (
     <>
       <div className="flex flex-1 justify-center w-full">
         <div className="space-y-4 md:space-y-6 sm:p-8 w-11/12">
           <h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl mb-5">
-            {$register ? 'Introduzca sus datos' : 'Inicia sesión en su cuenta'}
+            {$registerState
+              ? 'Introduzca sus datos'
+              : 'Inicia sesión en su cuenta'}
           </h1>
-          <form className="space-y-4 md:space-y-6" action="#">
-            <div className="w-full mb-7">
+          <form onSubmit={handleSubmit(onSubmit)} className="" action="#">
+            <div className="w-full">
               <span className="p-float-label">
-                <InputMask
-                  id="dni"
-                  value={dni_nie}
-                  autoClear={false}
-                  mask="*9999999a"
-                  slotChar=""
+                DNI/NIE
+                <input
+                  id="dni_nie"
+                  type="text"
+                  {...register('dni_nie', { required: true })}
                   className="h-8 start-5 w-full"
+                  value={dni_nie}
                   onChange={(e) => {
                     handleDNI_NIE(e)
                   }}
+                  aria-invalid={errors.dni_nie ? 'true' : 'false'}
                 />
-                <label htmlFor="dni">DNI/NIE</label>
+                {errors.dni_nie?.type === 'required' && (
+                  <small className="text-red-400 text-xs" role="alert">
+                    Este campo es obligatorio
+                  </small>
+                )}
               </span>
-              <div className="flex w-full justify-center mt-1">
+              <div className="flex w-full justify-start mt-1">
                 {errorDNI && (
                   <small className="text-red-400 text-xs">{errorDNI}</small>
                 )}
               </div>
             </div>
-            <div className="w-full mb-7">
-              <span className="p-float-label">
+            <div className="w-full mt-5">
+              <span className="">
+                Número de teléfono
                 <InputMask
-                  id="phoneNumber"
+                  id="telefono"
                   mask="(+99) 999 999 999"
+                  {...register('telefono', { required: true })}
                   slotChar=""
                   autoClear={false}
                   className="h-8 start-5 w-full"
                   value={phone}
-                  onChange={(e) => {
-                    setPhone(e.target.value)
-                  }}
+                  onChange={(e) => setPhone(e.target.value)}
                 />
-                <label htmlFor="phoneNumber">Número de teléfono</label>
+                {errors.telefono?.type === 'required' && (
+                  <small className="text-red-400 text-xs" role="alert">
+                    Este campo es obligatorio
+                  </small>
+                )}
+                {/* <label htmlFor="phoneNumber">Número de teléfono</label> */}
               </span>
             </div>
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between mt-5">
               <div className="flex items-start">
                 <div className="flex items-center h-5">
                   <input
                     id="remember"
+                    {...register('remember')}
                     aria-describedby="remember"
                     type="checkbox"
-                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
+                    className="accent-asiseg-blue w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-asiseg-blue "
                     required=""
                   />
                 </div>
@@ -111,20 +133,16 @@ export default function RegisterForm() {
                 className="text-sm font-medium text-primary-600 hover:underline "
               ></a>
             </div>
-            <div className="flex flex-1 justify-center">
-              <button
+            <div className="flex flex-1 justify-center mt-2">
+              <input
                 type="submit"
-                className=" text-white bg-asiseg-blue opacity-65 hover:opacity-100 transition-opacity p-2 rounded-md mb-4"
-                onClick={(e) => {
-                  handleSubmit(e)
-                }}
-              >
-                {$register ? 'Registrarse' : 'Iniciar sesión'}
-              </button>
+                value={$registerState ? 'Registrarse' : 'Iniciar sesión'}
+                className=" text-white bg-asiseg-blue opacity-65 hover:opacity-100 transition-opacity p-2 rounded-md mb-4 cursor-pointer  "
+              />
             </div>
             <div className="flex flex-row w-full justify-start items-center">
               <p className="text-sm font-light text-gray-500">
-                {$register
+                {$registerState
                   ? '¿Ya tienes una cuenta?'
                   : '¿No tienes aún una cuenta?'}
 
@@ -133,7 +151,7 @@ export default function RegisterForm() {
                   className="font-medium text-primary-600 hover:underline hover:text-asiseg-blue mx-1"
                   onClick={(e) => setRegister(e)}
                 >
-                  {$register ? 'Inicia sesión' : 'Regístrese'}
+                  {$registerState ? 'Inicia sesión' : 'Regístrese'}
                 </a>
               </p>
             </div>

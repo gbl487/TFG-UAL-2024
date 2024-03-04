5abc9a73cf465a511cc37d7bada8fd59f8219fd6
gbl487 <gbl487@inlumine.ual.es>
2024-03-05 00:09:52 +0100

    Formulario para creacion de tarjetas y configuracion de mockup de tarjeta

--- src/Views/components/CrearTarjeta.jsx
+++ src/Views/components/CrearTarjeta.jsx
@@ -1,19 +1,20 @@
-// import { useForm } from 'react-hook-form'
 import { useRef, useState } from 'react'
-import Card from './Card'
-import './quill.css'
 import { Editor } from 'primereact/editor'
-import { SeeMoreIcon } from '@Icons/Icons'
 import { FILTROS } from 'src/constants'
+import { useForm } from 'react-hook-form'
+import InfoCard from './InfoCard'
+import { deltaToHtml } from 'src/Controllers/utils/delta'
 export default function CrearTarjeta() {
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
   const [titulo, setTitulo] = useState()
-  const [fichero, setFichero] = useState()
   const [text, setText] = useState('')
+  const [content, setContent] = useState('')
+  const [desc, setDesc] = useState('')
+  const [tags, setTags] = useState([])
   const quillRef = useRef()
   const CabeceraEditor = () => {
     return (
@@ -22,65 +23,37 @@ export default function CrearTarjeta() {
           <button className="ql-bold" aria-label="Bold"></button>
           <button className="ql-italic" aria-label="Italic"></button>
           <button className="ql-underline" aria-label="Underline"></button>
+          <button className="ql-image" aria-label="Image"></button>
         </span>
       </div>
     )
   }
   const cabecera = CabeceraEditor()
-  const Footer = () => {
-    return (
-      <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-asiseg-blue rounded-lg opacity-65 hover:opacity-100 transition-opacity ">
-        Ver más
-        <SeeMoreIcon />
-      </div>
-    )
-  }
-  const obtenerContenido = () => {
-    const newDelta = quillRef.current.getQuill().editor.delta
-    const newHtml = deltaToHtml(newDelta)
-    setText(newHtml)
-  }
 
-  function deltaToHtml(delta) {
-    let html = ''
-    let newInsert = ''
-    // let string = ''
-    // const patron = /\n{2,}/g
-    delta.ops.forEach((op) => {
-      if (op.insert) {
-        if (typeof op.insert === 'string') {
-          // if (patron.test(op.insert)) {
-          //   string = op.insert
-          //   string.replace(patron, '\n')
-          //   console.log(string)
-          // }
-          if (op.insert !== '\n') {
-            newInsert = op.insert.replace(/\n/g, '</br>')
-            html += `<p>${newInsert}</p>` // Para texto
-          }
-        } else if (typeof op.insert === 'object' && op.insert.image) {
-          html += `<div class="flex justify-center">
-          <img src="${op.insert.image}"></div>` // Para imágenes
-        }
-      }
-      // Verifica los atributos del op
-      if (op.attributes) {
-        if (op.attributes.link) {
-          html = `<a href="${op.attributes.link}">${html}</a>`
-        }
-        if (op.attributes.italic) {
-          html = `<em>${html}</em>`
-        }
-        if (op.attributes.bold) {
-          html = `<strong>${html}</strong>`
-        }
-        if (op.attributes.underline) {
-          html = `<u>${html}</u>`
-        }
-      }
-    })
+  function getHtml(delta) {
+    setContent(delta)
+    const html = deltaToHtml(delta)
+    setText(html)
+    setDesc(html.replace(/<[^>]+>/g, ''))
+  }
+  function handleTagChange(e) {
+    const checkboxID = e.id
+    const isChecked = e.checked
 
-    return html
+    // Si el checkbox está marcado, agregamos el ID a la lista de tags
+    if (isChecked) {
+      if (!tags.includes(checkboxID)) {
+        setTags((prevTags) => [...prevTags, checkboxID])
+      }
+    } else {
+      // Si el checkbox está desmarcado, eliminamos el ID de la lista de tags
+      setTags((prevTags) => prevTags.filter((tag) => tag !== checkboxID))
+    }
+  }
+  const onSubmit = async (data) => {
+    console.log(data)
+    console.log(quillRef.current.getQuill().editor.delta)
+    console.log(content)
   }
 
   return (
@@ -91,7 +64,8 @@ export default function CrearTarjeta() {
             Por favor, introduzca todos los valores de todos los siguientes
             campos:
           </h1>
-          <form action="#">
+
+          <form onSubmit={handleSubmit(onSubmit)} action="#">
             <label className="form-control w-full max-w-md">
               <div className="label">
                 <span className="label-text text-black text-lg">Título</span>
@@ -99,11 +73,17 @@ export default function CrearTarjeta() {
               <input
                 type="text"
                 id="titulo"
+                {...register('titulo', { required: false })}
                 placeholder="Cura..."
-                value={titulo}
+                // value={titulo}
                 onChange={(e) => setTitulo(e.target.value)}
-                className="input bg-asiseg-gray/20 input-bordered w-full max-w-md"
+                className="input bg-asiseg-gray/10 input-bordered w-full max-w-md"
               />
+              {errors.titulo?.type === 'required' && (
+                <small className="text-red-400 text-xs" role="alert">
+                  Este campo es obligatorio
+                </small>
+              )}
             </label>
             <div className="mt-5">
               <div className="label">
@@ -112,76 +92,55 @@ export default function CrearTarjeta() {
               <input
                 type="file"
                 id="portada"
-                value={fichero}
-                onChange={(e) => setFichero(e.target.value)}
-                className="file-input bg-asiseg-gray/20 w-full max-w-md"
+                {...register('portada', { required: false })}
+                // value={fichero}
+                // onChange={(e) => setFichero(e.target.value)}
+                className="file-input bg-asiseg-gray/10 w-full max-w-md"
               />
             </div>
-            <div className="form-control w-full max-w-md mt-5">
+            <div className="form-control w-full max-w-lg mt-5">
               <div className="label">
                 <span className="label-text text-black text-lg">
                   Categorías
                 </span>
               </div>
-              <div className="flex flex-row">
-                <div className="flex flex-col mx-2">
-                  <div className="form-control bg-asiseg-gray/10 p-2 rounded-lg">
-                    <p>
-                      <strong>Tipo de Cura</strong>
-                    </p>
-                    <label htmlFor="PREOP" className="label cursor-pointer">
-                      <span className="label-text text-black pr-2">
-                        Preoperatorio
-                      </span>
-                      <input
-                        id="PREOP"
-                        type="checkbox"
-                        className="checkbox checkbox-primary"
-                      />
-                    </label>
-                    <label htmlFor="POSTOP" className="label cursor-pointer">
-                      <span className="label-text text-black pr-2">
-                        Postoperatorio
-                      </span>
-                      <input
-                        id="POSTOP"
-                        type="checkbox"
-                        className="checkbox checkbox-primary"
-                      />
-                    </label>
-                  </div>
-                </div>
-                <div className="flex flex-col mx-2">
-                  <div className="form-control bg-asiseg-gray/10 p-2 rounded-lg">
-                    <p>
-                      <strong>Área afectada</strong>
-                    </p>
-                    <label className="label cursor-pointer">
-                      <span className="label-text text-black pr-2">
-                        Corazón
-                      </span>
-                      <input
-                        type="checkbox"
-                        className="checkbox checkbox-primary"
-                      />
-                    </label>
-                    <label className="label cursor-pointer">
-                      <span className="label-text text-black pr-2">Pulmón</span>
-                      <input
-                        type="checkbox"
-                        className="checkbox checkbox-primary"
-                      />
-                    </label>
-
-                    <label className="label cursor-pointer">
-                      <span className="label-text text-black pr-2">Hígado</span>
-                      <input
-                        type="checkbox"
-                        className="checkbox checkbox-primary"
-                      />
-                    </label>
-                  </div>
-                </div>
+              <div className="flex flex-row flex-wrap gapx-2 gap-y-5">
+                {FILTROS.map((filtro) => {
+                  return (
+                    <div
+                      key={filtro.id}
+                      className="form-control bg-asiseg-gray/10 p-2 w-auto rounded-lg mx-2"
+                    >
+                      <p>
+                        <strong>{filtro.placeholder}</strong>
+                      </p>
+                      {filtro.options.map((categoria) => {
+                        return (
+                          <>
+                            <label
+                              key={categoria.code}
+                              htmlFor={categoria.code}
+                              className="label cursor-pointer"
+                            >
+                              <span className="label-text text-black pr-2">
+                                {categoria.name}
+                              </span>
+                              <input
+                                id={categoria.code}
+                                {...register('cat_' + categoria.code, {
+                                  required: false,
+                                })}
+                                onChange={(e) => handleTagChange(e.target)}
+                                type="checkbox"
+                                className="checkbox checkbox-primary"
+                              />
+                            </label>
+                          </>
+                        )
+                      })}
+                    </div>
+                  )
+                })}
               </div>
             </div>
             <div className="mt-5">
@@ -189,15 +148,23 @@ export default function CrearTarjeta() {
                 <span className="label-text text-black text-lg">Contenido</span>
               </div>
               <Editor
-                id="editor"
+                id="contenido"
                 // value={text}
                 ref={quillRef}
-                // onTextChange={(e) => handleEditorChange(e)}
-                className="max-w-3xl"
+                onTextChange={() =>
+                  getHtml(quillRef.current.getQuill().editor.delta)
+                }
+                className="max-w-4xl"
                 headerTemplate={cabecera}
-                style={{ height: '400px' }}
+                style={{ height: '600px' }}
               />
-              <button onClick={obtenerContenido}>Obtener Delta</button>
+              {/* <button onClick={obtenerContenido}>Obtener Delta</button> */}
+            </div>
+            <div className="flex flex-1 justify-center mt-10">
+              <input
+                type="submit"
+                className="btn btn-primary text-white opacity-65 transition-opacity p-4 rounded-md mb-4"
+              />
             </div>
           </form>
         </div>
@@ -205,13 +172,11 @@ export default function CrearTarjeta() {
         <div className="flex flex-col w-full basis-1/3 gap-y-5">
           <h1>Ejemplo</h1>
           <div className="flex justify-center">
-            <Card titulo={titulo} descripcion={text} Footer={Footer} />
-          </div>
-
-          <div>
-            <div
-              className=" inner"
-              dangerouslySetInnerHTML={{ __html: text }}
+            <InfoCard
+              titulo={titulo}
+              descripcion={desc}
+              tags={tags}
+              contenido={text}
             />
           </div>
         </div>
